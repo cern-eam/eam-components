dbc1dcbfe2d73b00397c7cca93fc6529f1cde492
Pedro Amaro <pedro.amaro@cern.ch>
2020-08-06 16:42:46 +0200

    [EAM-201] Added extra themes, kept 'theme' for backwards compatibility

--- dist/ui/components/theme/index.js
+++ dist/ui/components/theme/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.theme = void 0;
+exports.DANGER = exports.RED = exports.GREEN = exports.BLUE = exports.theme = void 0;
 
 var _styles = require("@material-ui/core/styles");
 
@@ -13,7 +13,13 @@ var _lightBlue = _interopRequireDefault(require("@material-ui/core/colors/lightB
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var theme = (0, _styles.createMuiTheme)({
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var muiSettings = {
   palette: {
     primary: {
       main: _blue["default"][500]
@@ -36,5 +42,30 @@ var theme = (0, _styles.createMuiTheme)({
       }
     }
   }
-});
+};
+var theme = (0, _styles.createMuiTheme)(muiSettings);
 exports.theme = theme;
+var BLUE = (0, _styles.createMuiTheme)(_objectSpread({}, muiSettings, {
+  custom: {
+    topBarColor: '#00aaff'
+  }
+}));
+exports.BLUE = BLUE;
+var GREEN = (0, _styles.createMuiTheme)(_objectSpread({}, muiSettings, {
+  custom: {
+    topBarColor: '#6ac860'
+  }
+}));
+exports.GREEN = GREEN;
+var RED = (0, _styles.createMuiTheme)(_objectSpread({}, muiSettings, {
+  custom: {
+    topBarColor: '#c86060'
+  }
+}));
+exports.RED = RED;
+var DANGER = (0, _styles.createMuiTheme)(_objectSpread({}, muiSettings, {
+  custom: {
+    topBarColor: '#ff0000'
+  }
+}));
+exports.DANGER = DANGER;
--- src/ui/components/theme/index.js
+++ src/ui/components/theme/index.js
@@ -2,7 +2,7 @@ import {createMuiTheme} from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
 import lightBlue from '@material-ui/core/colors/lightBlue';
 
-export const theme = createMuiTheme({
+const muiSettings = {
     palette: {
         primary: {main: blue[500]}, //EAMLight blue '#2196F3'
         secondary: {main: lightBlue[900]} //Darker blue '#01579b'
@@ -18,5 +18,35 @@ export const theme = createMuiTheme({
                 color: "#585858"
             }
         }
+    }
+};
+
+export const theme = createMuiTheme(muiSettings);
+
+export const BLUE = createMuiTheme({
+    ...muiSettings,
+    custom: {
+        topBarColor: '#00aaff'
+    }
+});
+
+export const GREEN = createMuiTheme({
+    ...muiSettings,
+    custom: {
+        topBarColor: '#6ac860'
+    }
+});
+
+export const RED = createMuiTheme({
+    ...muiSettings,
+    custom: {
+        topBarColor: '#c86060'
+    }
+});
+
+export const DANGER = createMuiTheme({
+    ...muiSettings,
+    custom: {
+        topBarColor: '#ff0000'
     }
 });
