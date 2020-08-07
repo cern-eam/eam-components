f2b9ea8730671f5e047130cca7cb69302d49ce7d
Pedro Amaro <pedro.amaro@cern.ch>
2020-08-07 11:29:48 +0200

    [EAM-201] Minor refactoring

--- src/ui/components/theme/index.js
+++ src/ui/components/theme/index.js
@@ -1,11 +1,15 @@
 import {createMuiTheme} from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
+import green from '@material-ui/core/colors/green';
+import red from '@material-ui/core/colors/red';
 import lightBlue from '@material-ui/core/colors/lightBlue';
+import lightGreen from '@material-ui/core/colors/lightGreen';
+import deepOrange from '@material-ui/core/colors/deepOrange';
 
-const muiSettings = primaryColor => ({
+const muiSettings = ({ primaryColor, secondaryColor }) => ({
     palette: {
         primary: {main: primaryColor}, //EAMLight blue '#2196F3'
-        secondary: {main: lightBlue[900]} //Darker blue '#01579b'
+        secondary: {main: secondaryColor} //Darker blue '#01579b'
     },
     typography: {
         body1: {
@@ -21,9 +25,16 @@ const muiSettings = primaryColor => ({
     }
 });
 
-export const theme = createMuiTheme(muiSettings('#2196F3'));
+export const theme = createMuiTheme({ primaryColor: blue[500], secondaryColor: lightBlue[900] });
 
-export const BLUE = createMuiTheme(muiSettings('#00aaff'));
-export const GREEN = createMuiTheme(muiSettings('#6ac860'));
-export const RED = createMuiTheme(muiSettings('#c86060'));
-export const DANGER = createMuiTheme(muiSettings('#ff0000'));
+const BLUE = createMuiTheme(muiSettings({ primaryColor: blue[500], secondaryColor: lightBlue[900] }));
+const GREEN = createMuiTheme(muiSettings({ primaryColor: green[600], secondaryColor: lightGreen[900] }));
+const RED = createMuiTheme(muiSettings({ primaryColor: red[500], secondaryColor: deepOrange[900] }));
+const DANGER = createMuiTheme(muiSettings({ primaryColor: red[900], secondaryColor: red[500] }));
+
+export default {
+    BLUE,
+    GREEN,
+    RED,
+    DANGER
+}
