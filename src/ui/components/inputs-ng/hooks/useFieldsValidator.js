8983e088aaa9e87ab60692aaa612067f9ce8a4d8
António Paulo <antonio.paulo@cern.ch>
2022-12-02 16:56:34 +0100

    [EAM-2286] Correct typos

--- src/ui/components/inputs-ng/hooks/useFieldsValidator.js
+++ src/ui/components/inputs-ng/hooks/useFieldsValidator.js
@@ -4,17 +4,17 @@ import { isRequired } from '../tools/input-tools';
 const BLANK_ERROR = ' cannot be blank';
 
 /**
- * Validates fields and generates the error messages to be shown (typically through the an errorText prop).
+ * Validates fields and generates the error messages to be shown (typically through an 'errorText' prop).
  * @param  {Object} fieldsData Contains `<K,V>` pairs of `<valueKey,fieldLayout>`
  * where `valueKey` is the same key used to store the corresponding value in the parameter
  * `formValues` and `fieldLayout` contains metadata describing the field (used in generating the
  * error message and validating the field).
  * @param  {Object} formValues Where all the values for the form fields are being kept.
  * It contains `<K,V>` pairs where `V` is the current field value (usually passed as prop to the field)
- * and `K` is the same key as the one passed to the corresponding entry in `requiredFieldsData`.
+ * and `K` is the same key as the one passed to the corresponding entry in `fieldsData`.
  * @param  {String} errorString The text to show, in the generated error message, following the field label.
  * @return {Object} Returns `errorMessages` and `validateFields`. `errorMessages` contains the error
- * messages using keys that match the ones passed in `requiredFieldsData` and `formValues`.
+ * messages using keys that match the ones passed in `fieldsData` and `formValues`.
  * `validateFields` is the validation function to be used before submitting the form.
  */
 const useFieldsValidator = (
