aad1d1622bf2495a66a7c84795e39532ee512f1c
Iulian Alexandru <iulian.alexandru@cern.ch>
2023-07-14 15:43:40 +0200

    EAM Time Picker

--- dist/enums/Constants.js
+++ dist/enums/Constants.js
@@ -3,6 +3,7 @@ export default {
   DATE_FORMAT_DISPLAY: 'dd-MMM-yyyy',
   DATETIME_FORMAT_VALUE: 'dd-MMM-yyyy HH:mm',
   DATETIME_FORMAT_DISPLAY: 'dd-MMM-yyyy HH:mm',
+  TIME_FORMAT_DISPLAY: 'HH:mm',
   SORT_ASC: 'asc',
   SORT_DESC: 'desc'
 };
