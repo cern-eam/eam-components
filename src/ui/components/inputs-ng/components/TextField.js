b0ce5fca044e89ecedcc45bba7775521a483d89c
IIlliirriiaann <131949142+IIlliirriiaann@users.noreply.github.com>
2024-06-19 14:54:47 +0200

    [EAM-3195] Right align fix
    The text now align on the right only when there's an adornment on the input

--- src/ui/components/inputs-ng/components/TextField.js
+++ src/ui/components/inputs-ng/components/TextField.js
@@ -33,9 +33,9 @@ const fieldInvalid = {
     borderRadius: '5px',
 }
 
-const numericFieldStyle = {
+const numericInputAdornmentStyle = {
     textAlign: "right",
-    paddingRight: "calc (70px + padding-left)" // 70px is the width of the TextFieldTextAdornment component
+    paddingRight: "80px" // 70px for the width of the TextFieldTextAdornment component + 10px padding
 }
 
 const TextField = (props) => {
@@ -68,7 +68,7 @@ const TextField = (props) => {
                                     maxLength={maxLength}
                                     //TODO this is not the best solution as we are overriding onInput handler that could be potentially passed from inputProps
                                     onInput={uppercase ? onInputUpperCaseHandler : undefined}
-                                    style = {type === 'number' ? numericFieldStyle : {}}
+                                    style = {type === 'number' && endTextAdornment ? numericInputAdornmentStyle : {}}
                                     />
                     {!hideDescription &&<TextFieldDescription
                         description = {desc}
