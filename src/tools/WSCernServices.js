150f16808aaddbb18471ea3169db0947ae85a266
purquijo <131868858+purquijo@users.noreply.github.com>
2024-07-03 16:13:14 +0200

    [EAM-1675] ClientId added to WSCernServices

--- src/tools/WSCernServices.js
+++ src/tools/WSCernServices.js
@@ -1,10 +1,8 @@
 // @flow
 import ajax from './ajax';
 
-/**
- * Handles all calls to REST Api
- */
 const baseUrl = '/apis/cern-eam-services/rest';
+const clientIdExchange = 'cern-eam-services';
 
 class WSCernServices {
 
@@ -12,19 +10,19 @@ class WSCernServices {
     //
     //
     _get(url, config = {}) {
-        return ajax.get(baseUrl + url, config);
+        return ajax.get(baseUrl + url, {...config, clientIdExchange});
     }
 
     _post(url, data, config = {}) {
-        return ajax.post(baseUrl + url, data, config);
+        return ajax.post(baseUrl + url, data, {...config, clientIdExchange});
     }
 
     _put(url, data, config = {}) {
-        return ajax.put(baseUrl + url, data, config);
+        return ajax.put(baseUrl + url, data, {...config, clientIdExchange});
     }
 
     _delete(url, config = {}) {
-        return ajax.delete(baseUrl + url, config);
+        return ajax.delete(baseUrl + url, {...config, clientIdExchange});
     }
 
 }
