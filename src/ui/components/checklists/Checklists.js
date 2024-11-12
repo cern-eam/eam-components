9c02a3454cd4a95541e7a54d91e5532c622ea452
Luis Barriga Carrasco <luisbarrigacarrasco@gmail.com>
2024-11-12 13:28:45 +0100

    Refactor Checklists to split code into utils file

--- src/ui/components/checklists/Checklists.js
+++ src/ui/components/checklists/Checklists.js
@@ -5,8 +5,24 @@ import React, {
   useMemo,
   useState,
 } from "react";
-import GridTools from "../grids/GridTools";
-import { getExpandedActivities, parseToBoolean } from "./utils/checklists";
+import {
+  concatActivityChecklistsToChecklists,
+  filterActivitiesWithChecklists,
+  getActivityCodeUrlParam,
+  getChecklistsEquipmentDisabled,
+  getCollapseFunction,
+  getEffectiveActivityCode,
+  getEffectiveEquipmentCode,
+  getExpandedActivities,
+  getFilledFilterChecklistsHidden,
+  getFilteredActivities,
+  getNewFilteredActivities,
+  getShowChecklistOptsUrlParam,
+  getShowFilledItemsUrlParam,
+  getTaskPlansMetadata,
+  getUpdatedChecklistsActivities,
+  parseToBoolean,
+} from "./utils/checklists";
 import SimpleEmptyState from "../emptystates/SimpleEmptyState";
 import BlockUi from "react-block-ui";
 import Collapse from "@mui/material/Collapse";
@@ -16,8 +32,6 @@ import WSChecklists from "../../../tools/WSChecklists";
 import FollowUpActivityDialog from "./dialogs/FollowUpActivityDialog";
 import ChecklistsOptions from "./ChecklistsOptions";
 import ChecklistsSelectors from "./ChecklistsSelectors";
-
-import { SIGNATURE_TYPES } from "./constants/signatures";
 import ChecklistsActivity from "./ChecklistsActivity";
 import ChecklistsContext from "./contexts/ChecklistsContext";
 
@@ -49,98 +63,49 @@ const Checklists = ({
   const [blocking, setBlocking] = useState(true);
   const [createFollowUpActivity, setCreateFollowUpActivity] = useState(null);
   const [filteredActivity, setFilteredActivity] = useState(
-    GridTools.getURLParameterByName("activityCode")
+    getActivityCodeUrlParam()
   );
   const [filteredEquipment, setFilteredEquipment] = useState(null);
   const [checklistsHidden, setChecklistsHidden] = useState({});
   const [showChecklistOptions, setShowChecklistOptions] = useState(
-    parseToBoolean(
-      GridTools.getURLParameterByName("showChecklistOptions"),
-      false
-    )
+    parseToBoolean(getShowChecklistOptsUrlParam(), false)
   );
 
-  const showFilledItems = useMemo(
-    () =>
-      parseToBoolean(GridTools.getURLParameterByName("showFilledItems"), true),
-    []
-  );
+  const showFilledItems = useMemo(() => {
+    return parseToBoolean(getShowFilledItemsUrlParam(), true);
+  }, []);
 
-  const activitiesWithChecklists = useMemo(
-    () => activities.filter((activity) => activity.checklists?.length),
-    [activities]
-  );
-
-  const isEmptyState = useMemo(
-    () => activitiesWithChecklists.length === 0,
-    [activitiesWithChecklists]
-  );
+  const activitiesWithChecklists = useMemo(() => {
+    return filterActivitiesWithChecklists(activities);
+  }, [activities]);
 
   const filteredActivities = useMemo(() => {
-    return activities.filter(
-      (activity) =>
-        activity.checklists &&
-        activity.checklists.length > 0 &&
-        !(
-          filteredEquipment &&
-          activity.equipments[filteredEquipment] === undefined
-        ) &&
-        !(filteredActivity && activity.activityCode !== filteredActivity)
+    return getFilteredActivities(
+      activities,
+      filteredEquipment,
+      filteredActivity
     );
   }, [activities]);
 
   const setNewFilter = useCallback(
-    (filters) => {
-      const { activity, equipmentCode } = filters;
-      const activityCode = activity?.code;
-      const effectiveActivityCode =
-        activityCode === undefined ? filteredActivity : activityCode;
-      const effectiveEquipmentCode =
-        equipmentCode === undefined ? filteredEquipment : equipmentCode;
+    ({ activity, equipmentCode }) => {
+      const effectiveActivityCode = getEffectiveActivityCode(
+        filteredActivity,
+        activity?.code
+      );
+      const effectiveEquipmentCode = getEffectiveEquipmentCode(
+        filteredEquipment,
+        equipmentCode
+      );
 
-      let activityCollapsedPredicate = (activity, effectiveActivityCode) => {};
-      let equipmentCollapsedPredicate = (
-        equipmentCode,
+      const newActivities = getNewFilteredActivities(
+        activities,
+        effectiveActivityCode,
         effectiveEquipmentCode
-      ) => {};
-
-      if (effectiveActivityCode || effectiveEquipmentCode) {
-        // if we're filtering, collapse everything that is not equal to our filters
-        activityCollapsedPredicate = (activity) =>
-          activity.activityCode !== effectiveActivityCode &&
-          Object.keys(activity.equipments).every(
-            (equipmentCode2) => equipmentCode2 !== effectiveEquipmentCode
-          );
-
-        equipmentCollapsedPredicate = (equipmentCode) =>
-          effectiveEquipmentCode && equipmentCode !== effectiveEquipmentCode;
-      } else {
-        // if nothing is being filter, uncollapse everything,
-        // to prepare for calling the collapse heuristic
-        activityCollapsedPredicate = () => false;
-        equipmentCollapsedPredicate = () => false;
-      }
-
-      const newActivities = activities.map((activity) => ({
-        ...activity,
-        collapsed: activityCollapsedPredicate(activity),
-        equipments: Object.keys(activity.equipments).reduce(
-          (equipments, thisEquipmentCode) => {
-            equipments[thisEquipmentCode] = {
-              ...activity.equipments[thisEquipmentCode],
-              collapsed: equipmentCollapsedPredicate(thisEquipmentCode),
-            };
-            return equipments;
-          },
-          {}
-        ),
-      }));
+      );
 
       if (!effectiveActivityCode && !effectiveEquipmentCode) {
-        const checklists = newActivities.reduce(
-          (checklists, activity) => checklists.concat(activity.checklists),
-          []
-        );
+        const checklists = concatActivityChecklistsToChecklists(activities);
         collapse(checklists, newActivities);
       }
 
@@ -153,31 +118,16 @@ const Checklists = ({
 
   const collapse = useCallback(
     (checklists, activities) => {
-      const defaultCollapse = (checklists, activities) => {
-        // if there are less than maxExpandedChecklistItems checklists, do not collapse anything
-        if (checklists.length < maxExpandedChecklistItems) return;
-
-        // otherwise, collapse every activity and every equipment within each activity
-        activities.forEach((activity) => {
-          if (!activity.forceActivityExpansion) {
-            activity.collapse();
-            Object.values(activity.equipments).forEach((equipment) =>
-              equipment.collapse()
-            );
-          }
-        });
-      };
-
-      const functionToRun =
-        typeof collapseHeuristic === "function"
-          ? collapseHeuristic
-          : defaultCollapse;
+      const functionToRun = getCollapseFunction(
+        collapseHeuristic,
+        maxExpandedChecklistItems
+      );
       const filteredChecklists = checklists.filter(
         ({ checkListCode }) => !checklistsHidden[checkListCode]
       );
       functionToRun(filteredChecklists, activities);
     },
-    [collapseHeuristic, checklistsHidden]
+    [collapseHeuristic, checklistsHidden, maxExpandedChecklistItems]
   );
 
   const hideCreateFollowUpWODialog = useCallback(() => {
@@ -186,34 +136,7 @@ const Checklists = ({
 
   const toggleFilledFilter = useCallback(() => {
     setChecklistsHidden((prev) =>
-      Object.keys(prev).length > 0
-        ? {}
-        : Object.fromEntries(
-            activities
-              .map((activity) => activity.checklists)
-              .flat(1)
-              .map(
-                ({
-                  checkListCode,
-                  result,
-                  finding,
-                  numericValue,
-                  freeText,
-                  date,
-                  dateTime,
-                  entityCode,
-                }) => [
-                  checkListCode,
-                  result ||
-                    finding ||
-                    numericValue ||
-                    date ||
-                    dateTime ||
-                    entityCode ||
-                    freeText,
-                ]
-              )
-          )
+      getFilledFilterChecklistsHidden(prev, activities)
     );
   }, [activities]);
 
@@ -221,34 +144,20 @@ const Checklists = ({
     (workorder, refreshCollapse = true) => {
       getWorkOrderActivities(workorder).then((response) => {
         let expActivities = getExpandedActivities(response.body.data);
-        const checklists = expActivities.reduce(
-          (checklists, activity) => checklists.concat(activity.checklists),
-          []
-        );
-        const taskCodes = [
-          ...new Set(
-            expActivities.map((activity) => ({
-              code: activity.taskCode,
-              revision: activity.taskRev,
-            }))
-          ),
-        ];
-        isCernMode &&
-          Promise.all(
-            taskCodes.map(
-              async (taskCode) =>
-                await getTaskPlanInstructions(taskCode.code, taskCode.revision)
-            )
-          ).then((responses) => {
-            const taskPlansMetadata = responses.reduce((acc, response) => {
-              let data = response.body.data;
-              acc[data.taskPlanCode] = data;
-              return acc;
-            }, {});
-            setTaskPlansMetadata(taskPlansMetadata);
-          });
+
+        if (isCernMode) {
+          const taskPlansMetadata = getTaskPlansMetadata(
+            expActivities,
+            getTaskPlanInstructions
+          );
+          setTaskPlansMetadata(taskPlansMetadata);
+        }
 
         if (refreshCollapse) {
+          const checklists = expActivities.reduce(
+            (checklists, activity) => checklists.concat(activity.checklists),
+            []
+          );
           collapse(checklists, expActivities);
         } else {
           expActivities = expActivities.map((activity, index) => ({
@@ -257,12 +166,9 @@ const Checklists = ({
           }));
         }
 
-        if (!showFilledItems) {
-          toggleFilledFilter();
-        }
-        if (activity) {
-          setNewFilter({ activity: { code: activity } });
-        }
+        if (!showFilledItems) toggleFilledFilter();
+        if (activity) setNewFilter({ activity: { code: activity } });
+
         setActivities(expActivities);
         setBlocking(false);
       });
@@ -303,47 +209,20 @@ const Checklists = ({
       if (checklistItem.conditional) {
         readActivities(checklistItem.workOrderCode, false);
       }
-      const activityCode = checklistItem.activityCode;
-      const checkListCode = checklistItem.checkListCode;
 
-      setActivities((prev) => {
-        const activities = [...prev];
-        const activityIndex = activities.findIndex(
-          (activity) => activity.activityCode === activityCode
-        );
-        const activity = { ...activities[activityIndex] };
-        activities[activityIndex] = activity;
-
-        const checklists = [...activity.checklists];
-        const checklistIndex = checklists.findIndex(
-          (checklistItem) => checklistItem.checkListCode === checkListCode
-        );
-        checklists[checklistIndex] = { ...checklistItem };
-        activity.checklists = checklists;
-
-        return activities;
-      });
+      setActivities((prev) =>
+        getUpdatedChecklistsActivities(prev, checklistItem)
+      );
     },
     [readActivities]
   );
 
   const handleChecklistsEquipmentDisabled = useCallback(
-    ({ signatures }) => {
-      return (
-        disabled ||
-        (signatures &&
-          signatures[SIGNATURE_TYPES.PERFORMER_1] &&
-          !signatures[SIGNATURE_TYPES.PERFORMER_1].viewAsPerformer &&
-          signatures[SIGNATURE_TYPES.PERFORMER_2] &&
-          !signatures[SIGNATURE_TYPES.PERFORMER_2].viewAsPerformer)
-      );
-    },
+    ({ signatures }) => getChecklistsEquipmentDisabled(disabled, signatures),
     [disabled]
   );
 
-  useEffect(() => {
-    readActivities(workorder);
-  }, [workorder, version]);
+  useEffect(() => readActivities(workorder), [workorder, version]);
 
   useEffect(() => {
     if (Object.keys(checklistsHidden).length === 0) {
@@ -354,7 +233,7 @@ const Checklists = ({
     }
   }, [checklistsHidden, filteredActivity, filteredEquipment]);
 
-  if (!blocking && isEmptyState) {
+  if (!blocking && activitiesWithChecklists.length === 0) {
     return <SimpleEmptyState message="No Checklists to show." />;
   }
 
--- src/ui/components/checklists/utils/checklists.js
+++ src/ui/components/checklists/utils/checklists.js
@@ -1,23 +1,26 @@
+import GridTools from "../../grids/GridTools";
+import { SIGNATURE_TYPES } from "../constants/signatures";
+
 export const parseToBoolean = (value, defaultValue) => {
   return value.length === 0 ? defaultValue : value !== "false";
 };
 
-export const getExpandedActivities = (activities) => {
-  const makeEquipmentsFromActivity = (activity) =>
-    activity.checklists.reduce((equipments, checklist) => {
-      if (equipments[checklist.equipmentCode] === undefined) {
-        equipments[checklist.equipmentCode] = {
-          code: checklist.equipmentCode,
-          desc: checklist.equipmentDesc,
-          collapse: function () {
-            this.collapsed = true;
-          },
-          collapsed: false,
-        };
-      }
-      return equipments;
-    }, {});
+const makeEquipmentsFromActivity = (activity) =>
+  activity.checklists.reduce((equipments, checklist) => {
+    if (equipments[checklist.equipmentCode] === undefined) {
+      equipments[checklist.equipmentCode] = {
+        code: checklist.equipmentCode,
+        desc: checklist.equipmentDesc,
+        collapse: function () {
+          this.collapsed = true;
+        },
+        collapsed: false,
+      };
+    }
+    return equipments;
+  }, {});
 
+export const getExpandedActivities = (activities) => {
   return activities.map((activity, index) => ({
     ...activity,
     index,
@@ -27,4 +30,206 @@ export const getExpandedActivities = (activities) => {
     },
     collapsed: false,
   }));
+};
+
+export const getActivityCodeUrlParam = () =>
+  GridTools.getURLParameterByName("activityCode");
+
+export const getShowChecklistOptsUrlParam = () =>
+  GridTools.getURLParameterByName("showChecklistOptions");
+
+export const getShowFilledItemsUrlParam = () =>
+  GridTools.getURLParameterByName("showFilledItems");
+
+export const filterActivitiesWithChecklists = (activities) =>
+  activities.filter((activity) => activity.checklists.length > 0);
+
+export const getFilteredActivities = (
+  activities,
+  filteredEquipment,
+  filteredActivity
+) => {
+  return activities.filter(
+    (activity) =>
+      activity.checklists &&
+      activity.checklists.length > 0 &&
+      !(
+        filteredEquipment &&
+        activity.equipments[filteredEquipment] === undefined
+      ) &&
+      !(filteredActivity && activity.activityCode !== filteredActivity)
+  );
+};
+
+export const getNewFilteredActivities = (
+  activities,
+  effectiveActivityCode,
+  effectiveEquipmentCode
+) => {
+  if (effectiveActivityCode || effectiveEquipmentCode) {
+    // if we're filtering, collapse everything that is not equal to our filters
+    return activities.map((activity) => ({
+      ...activity,
+      collapsed:
+        activity.activityCode !== effectiveActivityCode &&
+        Object.keys(activity.equipments).every(
+          (equipmentCode2) => equipmentCode2 !== effectiveEquipmentCode
+        ),
+      equipments: Object.keys(activity.equipments).reduce(
+        (equipments, thisEquipmentCode) => {
+          equipments[thisEquipmentCode] = {
+            ...activity.equipments[thisEquipmentCode],
+            collapsed:
+              effectiveEquipmentCode &&
+              thisEquipmentCode !== effectiveEquipmentCode,
+          };
+          return equipments;
+        },
+        {}
+      ),
+    }));
+  }
+  // if nothing is being filter, uncollapse everything,
+  // to prepare for calling the collapse heuristic
+  return activities.map((activity) => ({
+    ...activity,
+    collapsed: false,
+    equipments: Object.keys(activity.equipments).reduce(
+      (equipments, thisEquipmentCode) => {
+        equipments[thisEquipmentCode] = {
+          ...activity.equipments[thisEquipmentCode],
+          collapsed: false,
+        };
+        return equipments;
+      },
+      {}
+    ),
+  }));
+};
+
+export const getEffectiveActivityCode = (filteredActivity, activityCode) => {
+  return activityCode === undefined ? filteredActivity : activityCode;
+};
+
+export const getEffectiveEquipmentCode = (filteredEquipment, equipmentCode) => {
+  return equipmentCode === undefined ? filteredEquipment : equipmentCode;
+};
+
+export const concatActivityChecklistsToChecklists = (activities) => {
+  return activities.reduce(
+    (checklists, activity) => checklists.concat(activity.checklists),
+    []
+  );
+};
+
+export const getCollapseFunction = (
+  collapseHeuristic,
+  maxExpandedChecklistItems
+) => {
+  if (typeof collapseHeuristic === "function") return collapseHeuristic;
+  return (checklists, activities) => {
+    // if there are less than maxExpandedChecklistItems checklists, do not collapse anything
+    if (checklists.length < maxExpandedChecklistItems) return;
+
+    // otherwise, collapse every activity and every equipment within each activity
+    activities.forEach((activity) => {
+      if (!activity.forceActivityExpansion) {
+        activity.collapse();
+        Object.values(activity.equipments).forEach((equipment) =>
+          equipment.collapse()
+        );
+      }
+    });
+  };
+};
+
+export const getFilledFilterChecklistsHidden = (
+  checklistsHidden,
+  activities
+) => {
+  if (Object.keys(checklistsHidden).length > 0) return {};
+  return Object.fromEntries(
+    activities
+      .map((activity) => activity.checklists)
+      .flat(1)
+      .map(
+        ({
+          checkListCode,
+          result,
+          finding,
+          numericValue,
+          freeText,
+          date,
+          dateTime,
+          entityCode,
+        }) => [
+          checkListCode,
+          result ||
+            finding ||
+            numericValue ||
+            date ||
+            dateTime ||
+            entityCode ||
+            freeText,
+        ]
+      )
+  );
+};
+
+export const getUpdatedChecklistsActivities = (activities, checklistItem) => {
+  const activityCode = checklistItem.activityCode;
+  const checkListCode = checklistItem.checkListCode;
+  const newActivities = [...activities];
+  const activityIndex = newActivities.findIndex(
+    (activity) => activity.activityCode === activityCode
+  );
+  const activity = { ...newActivities[activityIndex] };
+  newActivities[activityIndex] = activity;
+
+  const checklists = [...activity.checklists];
+  const checklistIndex = checklists.findIndex(
+    (checklistItem) => checklistItem.checkListCode === checkListCode
+  );
+  checklists[checklistIndex] = { ...checklistItem };
+  activity.checklists = checklists;
+
+  return newActivities;
+};
+
+export const getChecklistsEquipmentDisabled = (disabled, signatures) => {
+  return (
+    disabled ||
+    (signatures &&
+      signatures[SIGNATURE_TYPES.PERFORMER_1] &&
+      !signatures[SIGNATURE_TYPES.PERFORMER_1].viewAsPerformer &&
+      signatures[SIGNATURE_TYPES.PERFORMER_2] &&
+      !signatures[SIGNATURE_TYPES.PERFORMER_2].viewAsPerformer)
+  );
+};
+
+export const getTaskPlansMetadata = (
+  expActivities,
+  getTaskPlanInstructions
+) => {
+  const taskCodes = [
+    ...new Set(
+      expActivities.map((activity) => ({
+        code: activity.taskCode,
+        revision: activity.taskRev,
+      }))
+    ),
+  ];
+
+  Promise.all(
+    taskCodes.map(
+      async (taskCode) =>
+        await getTaskPlanInstructions(taskCode.code, taskCode.revision)
+    )
+  ).then((responses) => {
+    return responses.reduce((acc, response) => {
+      let data = response.body.data;
+      acc[data.taskPlanCode] = data;
+      return acc;
+    }, {});
+  });
 };
